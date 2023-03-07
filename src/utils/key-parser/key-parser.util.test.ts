6472387ed4a7b426d23bff93de3036c1c73fa3a5
Omar Castro <omar.castro.360@gmail.com>
2023-03-07 23:47:56 +0000

    src: deno fmt key-parser

--- src/utils/key-parser/key-parser.util.test.ts
+++ src/utils/key-parser/key-parser.util.test.ts
@@ -1,64 +1,74 @@
+import { test } from '../../../test-utils/unit/test.ts'
+import { getAST, parseKey, states } from './key-parser.util.ts'
 
-import { test } from '../../../test-utils/unit/test.ts';
-import { parseKey, getAST, states } from './key-parser.util.ts';
+test('Given a simple string, getAST should return an AST with one token', async ({ step, expect }) => {
+	const ast = getAST('hello world')
+	expect(ast.tokens).toEqual([{
+		start: 0,
+		end: 11,
+		type: states.normal,
+		text: 'hello world',
+		childTokens: [],
+	}])
+})
 
-test("Given a simple string, getAST should return an AST with one token", async ({step, expect}) => {
-    
-    const ast = getAST("hello world");
-    expect(ast.tokens).toEqual([{ start: 0, end: 11, type: states.normal, text: "hello world", childTokens: []}])
+test('Given a simple template string, getAST should return an AST with 3 tokens', async ({ step, expect }) => {
+	const ast = getAST('hello {} world')
+	expect(ast.tokens).toEqual([
+		{ start: 0, end: 6, type: states.normal, text: 'hello ', childTokens: [] },
+		{ start: 7, end: 7, type: states.capture, text: '', childTokens: [] },
+		{ start: 8, end: 14, type: states.normal, text: ' world', childTokens: [] },
+	])
+})
 
-});
+test('Given a template string with "number" keyword, getAST should return an AST with 3 tokens', async ({ step, expect }) => {
+	const ast = getAST('I see {number} worlds')
+	expect(ast.tokens).toEqual([
+		{ start: 0, end: 6, type: states.normal, text: 'I see ', childTokens: [] },
+		{
+			start: 7,
+			end: 13,
+			type: states.capture,
+			text: 'number',
+			childTokens: [],
+		},
+		{
+			start: 14,
+			end: 21,
+			type: states.normal,
+			text: ' worlds',
+			childTokens: [],
+		},
+	])
+})
 
-test("Given a simple template string, getAST should return an AST with 3 tokens", async ({step, expect}) => {
-    
-    const ast = getAST("hello {} world");
-    expect(ast.tokens).toEqual([
-        { start: 0, end: 6, type: states.normal, text: "hello ", childTokens: [] },
-        { start: 7, end: 7, type: states.capture, text: "", childTokens: [] },
-        { start: 8, end: 14, type: states.normal, text: " world", childTokens: [] },
-    ])
+test('Given a string with escaped initial curly brace, getAST should return an AST with 1 tokens', async ({ step, expect }) => {
+	const ast = getAST('hello \\{} world')
+	expect(ast.tokens).toEqual([{
+		start: 0,
+		end: 15,
+		type: states.normal,
+		text: 'hello \\{} world',
+		childTokens: [],
+	}])
+})
 
-});
+test('Given a simple string, parseKey should return a result with max priority', async ({ step, expect }) => {
+	const parseKeyResult = parseKey('hello world')
+	const { priority, key } = parseKeyResult
 
+	expect({ priority, key }).toEqual({
+		priority: [1, 0, 0],
+		key: 'hello world',
+	})
+})
 
-test('Given a template string with "number" keyword, getAST should return an AST with 3 tokens', async ({step, expect}) => {
-    
-    const ast = getAST("I see {number} worlds");
-    expect(ast.tokens).toEqual([
-        { start: 0, end: 6, type: states.normal, text: "I see ", childTokens: [] },
-        { start: 7, end: 13, type: states.capture, text: "number", childTokens: [] },
-        { start: 14, end: 21, type: states.normal, text: " worlds", childTokens: [] },
-    ])
+test('Given a dynamic string, parseKey should return a result', async ({ step, expect }) => {
+	const parseKeyResult = parseKey('hello {}')
+	const { priority, key } = parseKeyResult
 
-});
-
-test('Given a string with escaped initial curly brace, getAST should return an AST with 1 tokens', async ({step, expect}) => {
-    
-    const ast = getAST("hello \\{} world");
-    expect(ast.tokens).toEqual([{ start: 0, end: 15, type: states.normal, text: "hello \\{} world", childTokens: []}])
-
-});
-
-test("Given a simple string, parseKey should return a result with max priority", async ({step, expect}) => {
-    
-    const parseKeyResult = parseKey("hello world");
-    const {priority, key} = parseKeyResult
-
-    expect({priority, key}).toEqual({
-        priority: [1,0,0],
-        key: "hello world"
-    })
-
-});
-
-test("Given a dynamic string, parseKey should return a result", async ({step, expect}) => {
-    
-    const parseKeyResult = parseKey("hello {}");
-    const {priority, key} = parseKeyResult
-
-    expect({priority, key}).toEqual({
-        priority: [0,0,1],
-        key: "hello {}"
-    })
-
-});
+	expect({ priority, key }).toEqual({
+		priority: [0, 0, 1],
+		key: 'hello {}',
+	})
+})
--- src/utils/key-parser/key-parser.util.ts
+++ src/utils/key-parser/key-parser.util.ts
@@ -1,48 +1,46 @@
-
 /// states
 export enum states {
-    normal = 0,
-    capture,
-    regex,
-    sq_string,
-    dq_string,
-    bt_string,
-    escape,
-    
-    previous
+	normal = 0,
+	capture,
+	regex,
+	sq_string,
+	dq_string,
+	bt_string,
+	escape,
+
+	previous,
 }
 
-const ch = (char:string) => char.charCodeAt(0)
+const ch = (char: string) => char.charCodeAt(0)
 const normalState = [] as states[]
-normalState[ch("{")] = states.capture
-normalState[ch("\\")] = states.escape
+normalState[ch('{')] = states.capture
+normalState[ch('\\')] = states.escape
 
 const captureState = [] as states[]
-captureState[ch("}")] = states.previous
-captureState[ch("/")] = states.regex
-captureState[ch("'")] = states.sq_string
+captureState[ch('}')] = states.previous
+captureState[ch('/')] = states.regex
+captureState[ch('\'')] = states.sq_string
 captureState[ch('"')] = states.dq_string
-captureState[ch("`")] = states.bt_string
-captureState[ch("\\")] = states.escape
+captureState[ch('`')] = states.bt_string
+captureState[ch('\\')] = states.escape
 
 const regexState = [] as states[]
-regexState[ch("/")] = states.previous
-regexState[ch("\\")] = states.escape
+regexState[ch('/')] = states.previous
+regexState[ch('\\')] = states.escape
 
 const singleQuoteStringState = [] as states[]
-singleQuoteStringState[ch("'")] = states.previous
-singleQuoteStringState[ch("\\")] = states.escape
+singleQuoteStringState[ch('\'')] = states.previous
+singleQuoteStringState[ch('\\')] = states.escape
 
 const doubleQuoteStringState = [] as states[]
 doubleQuoteStringState[ch('"')] = states.previous
-doubleQuoteStringState[ch("\\")] = states.escape
+doubleQuoteStringState[ch('\\')] = states.escape
 
 const backtickStringState = [] as states[]
-backtickStringState[ch("`")] = states.previous
-backtickStringState[ch("\\")] = states.escape
+backtickStringState[ch('`')] = states.previous
+backtickStringState[ch('\\')] = states.escape
 
-
-const escapeState = new Proxy([], {get: () => states.previous}) as states[]
+const escapeState = new Proxy([], { get: () => states.previous }) as states[]
 
 const stateMachine = [] as states[][]
 stateMachine[states.normal] = normalState
@@ -54,170 +52,164 @@ stateMachine[states.bt_string] = backtickStringState
 stateMachine[states.capture] = captureState
 stateMachine[states.escape] = escapeState
 
+export function getAST(key: string): AST {
+	let previousState = states.normal
+	let currentState = states.normal
+	let currentMachineState = stateMachine[currentState]
+	const tokens = []
+	const rootnode = {
+		tokens: [],
+	} as TmpAST
+	let currentToken = {
+		parentNode: rootnode,
+		start: 0,
+		end: 0,
+		type: states.normal,
+		childTokens: [],
+	} as TmpToken
 
-export function getAST(key: string): AST{
-    let previousState = states.normal
-    let currentState = states.normal;
-    let currentMachineState = stateMachine[currentState];
-    const tokens = [];
-    const rootnode = {
-        tokens: []
-    } as TmpAST
-    let currentToken = {
-        parentNode: rootnode,
-        start:0,
-        end: 0,
-        type: states.normal,
-        childTokens: []
-    } as TmpToken
+	const setCurrentState = (newState: states) => {
+		previousState = currentState
+		currentState = newState
+		currentMachineState = stateMachine[currentState]
+	}
 
-    const setCurrentState = (newState: states) => {
-        previousState = currentState
-        currentState = newState
-        currentMachineState = stateMachine[currentState]
-    }
+	const length = key.length
+	for (var i = 0; i < length; i++) {
+		const ch = key.charCodeAt(i)
 
-    const length = key.length
-    for (var i = 0; i < length; i++) {
-        const ch = key.charCodeAt(i);
+		const nextState = currentMachineState[ch]
+		if (nextState == null || nextState === currentState) continue
 
+		if (nextState === states.previous) {
+			setCurrentState(previousState)
+			if (currentToken.type === states.escape) {
+				if (currentToken.parentNode === rootnode) {
+					currentToken = rootnode.tokens.pop() as TmpToken
+					continue
+				}
+				currentToken = currentToken.parentNode as TmpToken
+				continue
+			}
 
-        const nextState = currentMachineState[ch]
-        if(nextState == null || nextState === currentState){ continue }
+			currentToken.end = i
+			if (currentToken.parentNode === rootnode) {
+				rootnode.tokens.push(currentToken)
+				currentToken = {
+					parentNode: rootnode,
+					start: i + 1,
+					end: i + 1,
+					type: states.normal,
+					childTokens: [],
+				}
+				continue
+			}
+			const parentNode = currentToken.parentNode as TmpToken
+			parentNode.childTokens.push(currentToken)
+			currentToken = parentNode
+			continue
+		}
+		switch (currentState) {
+			case states.normal:
+				currentToken.end = i
+				if (currentToken.end > currentToken.start) {
+					rootnode.tokens.push(currentToken)
+				}
+				currentToken = {
+					parentNode: rootnode,
+					start: i + 1,
+					end: i + 1,
+					type: nextState,
+					childTokens: [],
+				}
+				break
+			default:
+				const newToken = {
+					parentNode: currentToken,
+					start: i + 1,
+					end: i + 1,
+					type: nextState,
+					childTokens: [],
+				}
+				currentToken = newToken
+		}
+		setCurrentState(nextState)
+	}
+	currentToken.end = key.length
+	if (currentToken.end > currentToken.start) {
+		rootnode.tokens.push(currentToken)
+	}
+	const toToken: (a: TmpToken) => Token = (
+		{ start, end, type, childTokens }: TmpToken,
+	) => ({
+		start,
+		end,
+		type,
+		text: key.substring(start, end),
+		childTokens: childTokens.map(toToken),
+	})
 
-
-        if(nextState === states.previous){
-            setCurrentState(previousState)
-            if(currentToken.type === states.escape){
-                if(currentToken.parentNode === rootnode){
-                    currentToken = rootnode.tokens.pop() as TmpToken
-                    continue
-                }
-                currentToken = currentToken.parentNode as TmpToken
-                continue
-            }
-            
-            currentToken.end = i
-            if(currentToken.parentNode === rootnode){
-                rootnode.tokens.push(currentToken)
-                currentToken = {
-                    parentNode: rootnode,
-                    start:i + 1,
-                    end: i + 1,
-                    type: states.normal,
-                    childTokens: []
-                }
-                continue
-            }
-            const parentNode = currentToken.parentNode as TmpToken
-            parentNode.childTokens.push(currentToken)
-            currentToken = parentNode
-            continue
-        }
-        switch(currentState){
-            case states.normal:
-                currentToken.end = i
-                if(currentToken.end > currentToken.start){
-                    rootnode.tokens.push(currentToken)
-                }
-                currentToken = {
-                    parentNode: rootnode,
-                    start:i + 1,
-                    end: i + 1,
-                    type: nextState,
-                    childTokens: [],
-                }
-            break
-            default:
-
-                const newToken = {
-                    parentNode: currentToken,
-                    start:i + 1,
-                    end: i + 1,
-                    type: nextState,
-                    childTokens: [],
-                }
-                currentToken = newToken
-
-        }
-        setCurrentState(nextState)
-    }
-    currentToken.end = key.length
-    if(currentToken.end > currentToken.start){
-        rootnode.tokens.push(currentToken)
-    }
-    const toToken: (a: TmpToken) => Token = ({start, end, type, childTokens}: TmpToken) => ({
-        start, end, type,
-        text: key.substring(start, end),
-        childTokens: childTokens.map(toToken)
-    })
-    
-    return {
-        key,
-        tokens: rootnode.tokens.map(toToken)
-    };
+	return {
+		key,
+		tokens: rootnode.tokens.map(toToken),
+	}
 }
 
 export function parseKey(key: string) {
-    const result = {
-        priority: [0,0,0],
-        key
-    } as parseResult
+	const result = {
+		priority: [0, 0, 0],
+		key,
+	} as parseResult
 
-    const ast = getAST(key);
-    const captures = ast.tokens.filter(token => token.type === states.capture)
+	const ast = getAST(key)
+	const captures = ast.tokens.filter((token) => token.type === states.capture)
 
-    if(captures.length <= 0){
-        result.priority[0] = 1
-        result.matches = matchesEquality(key)
-        return result
-    }
+	if (captures.length <= 0) {
+		result.priority[0] = 1
+		result.matches = matchesEquality(key)
+		return result
+	}
 
-    result.priority[2] = 1
-    result.matches = matchesEquality(key)
-    return result
+	result.priority[2] = 1
+	result.matches = matchesEquality(key)
+	return result
 }
-
-
 
 /// matchers
 const matchesEquality = (key: string) => (text: string) => text === key
 
-
-
-
 /// types
 
 type parseResult = {
-    /**
-     * Defined the priority of parsing
-     */
-    priority: [number, number, number]
-    key: string
-    matches(text: string): boolean
+	/**
+	 * Defined the priority of parsing
+	 */
+	priority: [number, number, number]
+	key: string
+	matches(text: string): boolean
 }
 
 type TmpAST = {
-    tokens: TmpToken[]
+	tokens: TmpToken[]
 }
 
 type AST = {
-    key: string,
-    tokens: Token[]
+	key: string
+	tokens: Token[]
 }
 
 type TmpToken = {
-    parentNode: TmpAST | TmpToken
-    start: number
-    end: number
-    type: states
-    childTokens: TmpToken[]
+	parentNode: TmpAST | TmpToken
+	start: number
+	end: number
+	type: states
+	childTokens: TmpToken[]
 }
 
 type Token = {
-    start: number
-    end: number
-    type: states
-    text: string
-    childTokens: Token[]
+	start: number
+	end: number
+	type: states
+	text: string
+	childTokens: Token[]
 }
