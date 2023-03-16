15321c94e9c63432164e7c4188ce045972f51a40
Omar Castro <omar.castro.360@gmail.com>
2023-03-16 20:24:26 +0000

    test: hack jsdom to work on deno

--- buildfiles/test.sh
+++ buildfiles/test.sh
@@ -2,4 +2,4 @@
 
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$DIR/.."
-deno test --allow-read --allow-env --allow-net src
+deno test --allow-read --allow-env --allow-net --import-map=test-utils/unit/import_map.json src
