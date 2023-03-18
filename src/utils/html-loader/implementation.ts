64c7a63c9fc5bb66e7d5159ac4e4f885fa5fcb5e
Omar Castro <omar.castro.360@gmail.com>
2023-03-18 19:06:25 +0000

    src: add loadI18n implementation

--- src/utils/html-loader/implementation.ts
+++ src/utils/html-loader/implementation.ts
@@ -1,20 +1,42 @@
 import { importI18nJson } from "../i18n-importer/mod.ts"
-import { i18nTanslationStore, type TranslationStore } from "../store/translation-store.ts"
+import { i18nTanslationStore, type TranslationStore, type StoreData } from "../store/translation-store.ts"
 
-export function loadI18n(document: Document): Promise<TranslationStore> {
+
+
+type I18nMap = Awaited<ReturnType<typeof importI18nJson>>
+
+export async function loadI18n(document: Document): Promise<TranslationStore> {
+
 
   const store = i18nTanslationStore()
-  const deferrdPromises = [] as Promise<unknown>[]
+
+
+  const deferrdPromises = [] as Promise<I18nMap>[]
 
   const location = window.location.href;
 
-  document.querySelectorAll('link[rel="i18n-language-map"]').forEach(link => {
+  const storeData = {
+    location,
+    languages: {}
+  } as StoreData
+
+  
+  
+
+  document.querySelectorAll('link[rel="i18n-locale-map"]').forEach(link => {
     const href = link.getAttribute("href")!
-    const deferredLoad = importI18nJson(href, window.location.href).then(data => store.loadTranslations({languages: data, location: location}))
-    deferrdPromises.push(deferredLoad)
-    
+    deferrdPromises.push(importI18nJson(href, window.location.href))    
   })
 
-  return Promise.all(deferrdPromises).then(() => store)
-}
+  await Promise.allSettled(deferrdPromises).then((results) => results.forEach(result => {
+    if(result.status === "rejected"){
+      console.error("error loading file: %o", result.reason)
+      return
+    }
+    Object.entries(result.value.languages ?? {})
+    storeData.languages
+  }))
 
+
+  return store
+}
