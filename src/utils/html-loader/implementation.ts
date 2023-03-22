46319303b064d74229cff3dc4ecae0a9ff8d2ea5
Omar Castro <omar.castro.360@gmail.com>
2023-03-22 22:55:45 +0000

    src: format code

--- src/utils/html-loader/implementation.ts
+++ src/utils/html-loader/implementation.ts
@@ -1,42 +1,33 @@
-import { importI18nJson } from "../i18n-importer/mod.ts"
-import { i18nTanslationStore, type TranslationStore, type StoreData } from "../store/translation-store.ts"
-
-
-
-type I18nMap = Awaited<ReturnType<typeof importI18nJson>>
+import { type I18nDefinitionMap, importI18nJson } from '../i18n-importer/mod.ts'
+import { i18nTanslationStore, type StoreData, type TranslationStore } from '../store/translation-store.ts'
 
 export async function loadI18n(document: Document): Promise<TranslationStore> {
-
-
   const store = i18nTanslationStore()
 
+  const deferrdPromises = [] as Promise<I18nDefinitionMap>[]
 
-  const deferrdPromises = [] as Promise<I18nMap>[]
-
-  const location = window.location.href;
+  const location = window.location.href
 
   const storeData = {
     location,
-    languages: {}
+    languages: {},
   } as StoreData
 
-  
-  
-
-  document.querySelectorAll('link[rel="i18n-locale-map"]').forEach(link => {
-    const href = link.getAttribute("href")!
-    deferrdPromises.push(importI18nJson(href, window.location.href))    
+  document.querySelectorAll('link[rel="i18n-locale-map"]').forEach((link) => {
+    const href = link.getAttribute('href')!
+    deferrdPromises.push(importI18nJson(href, window.location.href))
   })
 
-  await Promise.allSettled(deferrdPromises).then((results) => results.forEach(result => {
-    if(result.status === "rejected"){
-      console.error("error loading file: %o", result.reason)
-      return
-    }
-    Object.entries(result.value.languages ?? {})
-    storeData.languages
-  }))
-
+  await Promise.allSettled(deferrdPromises).then((results) =>
+    results.forEach((result) => {
+      if (result.status === 'rejected') {
+        console.error('error loading file: %o', result.reason)
+        return
+      }
+      Object.entries(result.value.languages ?? {})
+      storeData.languages
+    })
+  )
 
   return store
 }
