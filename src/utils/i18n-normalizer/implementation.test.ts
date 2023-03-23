5c64fbfd9f4420b6f103614935ba9068078ec035
Omar Castro <omar.castro.360@gmail.com>
2023-03-23 22:50:58 +0000

    test: update tests

--- src/utils/i18n-normalizer/implementation.test.ts
+++ src/utils/i18n-normalizer/implementation.test.ts
@@ -1,12 +1,27 @@
 import { test } from '../../../test-utils/unit/test.ts'
 import { normalizeI18nDefinition } from './implementation.ts'
 
+test('Given an array, normalizeI18nDefinition normalizes to an tuple extends', ({ expect }) => {
+  const input = ['lang.en.json', 'customization.lang.en']
+  const result = normalizeI18nDefinition(input)
+  expect(result).toEqual({
+    result: {
+      extends: input,
+      translations: {},
+    },
+    errors: [],
+  })
+})
+
 test('Given a string, normalizeI18nDefinition normalizes to an singluar tuple extends', ({ expect }) => {
   const input = 'lang.en.json'
   const result = normalizeI18nDefinition(input)
   expect(result).toEqual({
-    extends: [input],
-    translations: {},
+    result: {
+      extends: [input],
+      translations: {},
+    },
+    errors: [],
   })
 })
 
@@ -14,7 +29,10 @@ test('Given an array, normalizeI18nDefinition normalizes to an tuple extends', (
   const input = ['lang.en.json', 'customization.lang.en']
   const result = normalizeI18nDefinition(input)
   expect(result).toEqual({
-    extends: input,
-    translations: {},
+    result: {
+      extends: input,
+      translations: {},
+    },
+    errors: [],
   })
 })
