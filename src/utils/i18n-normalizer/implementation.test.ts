af20e3886cc5717fb8c39dcf00fc6360afe7e175
Omar Castro <omar.castro.360@gmail.com>
2023-03-23 23:03:00 +0000

    test:add empty string test

--- src/utils/i18n-normalizer/implementation.test.ts
+++ src/utils/i18n-normalizer/implementation.test.ts
@@ -1,19 +1,16 @@
 import { test } from '../../../test-utils/unit/test.ts'
 import { normalizeI18nDefinition } from './implementation.ts'
 
-test('Given an array, normalizeI18nDefinition normalizes to an tuple extends', ({ expect }) => {
-  const input = ['lang.en.json', 'customization.lang.en']
+test('Given an empty string, normalizeI18nDefinition returns empty definition with error', ({ expect }) => {
+  const input = ''
   const result = normalizeI18nDefinition(input)
   expect(result).toEqual({
-    result: {
-      extends: input,
-      translations: {},
-    },
-    errors: [],
+    result: { extends: [], translations: {} },
+    errors: [{ path: '', message: 'cannot import empty path, ignoring extends' }],
   })
 })
 
-test('Given a string, normalizeI18nDefinition normalizes to an singluar tuple extends', ({ expect }) => {
+test('Given a valid string, normalizeI18nDefinition normalizes to an singluar tuple extends', ({ expect }) => {
   const input = 'lang.en.json'
   const result = normalizeI18nDefinition(input)
   expect(result).toEqual({
@@ -25,7 +22,7 @@ test('Given a string, normalizeI18nDefinition normalizes to an singluar tuple ex
   })
 })
 
-test('Given an array, normalizeI18nDefinition normalizes to an tuple extends', ({ expect }) => {
+test('Given an valid array, normalizeI18nDefinition normalizes to an tuple extends', ({ expect }) => {
   const input = ['lang.en.json', 'customization.lang.en']
   const result = normalizeI18nDefinition(input)
   expect(result).toEqual({
