01627e6ea77210d144224761ab17205a4b3f6326
Omar Castro <omar.castro.360@gmail.com>
2023-03-30 22:34:26 +0100

    src: add i18n-normalizer/mod.ts

---
+++ src/utils/i18n-normalizer/mod.ts
@@ -0,0 +1,8 @@
+export { normalizeI18nDefinition, normalizeI18nDefinitionMap } from './implementation.ts'
+export type {
+  I18nDefinition,
+  I18nDefinitionMap,
+  NormalizedI18nDefinition,
+  NormalizedI18nDefinitionMap,
+  Translations,
+} from './implementation.ts'
