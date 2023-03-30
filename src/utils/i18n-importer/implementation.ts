cdf152e8ea228669a2e793cefdc0cb137c65d863
Omar Castro <omar.castro.360@gmail.com>
2023-03-30 22:33:05 +0100

    reuse normalizeI18nDefinitionMap on i18n-importer

--- src/utils/i18n-importer/implementation.ts
+++ src/utils/i18n-importer/implementation.ts
@@ -1,48 +1,12 @@
-import type { I18nDefinition, I18nDefinitionMap, Translations } from './provider.ts'
+import type { I18nDefinitionMap, Translations } from './provider.ts'
+import { normalizeI18nDefinitionMap } from '../i18n-normalizer/mod.ts'
 import { provide } from './provider.ts'
-
-type I18nDefinitionMapResponse = {
-  [key: string]: Partial<I18nDefinition>
-}
 
 function isTranslationMap(json: Translations): json is Translations {
   if (typeof json !== 'object' || Array.isArray(json)) {
     return false
   }
   return Object.keys(json).every((key) => typeof [json[key]] === 'string')
-}
-
-function isi18nDefinition(definition: Partial<I18nDefinition>): definition is Partial<I18nDefinition> {
-  const { extends: xtends, translations } = definition
-  if (xtends == null) {
-    return true
-  }
-  if (typeof xtends === 'string') {
-    return true
-  }
-  if ((Array.isArray(xtends) && xtends.every((field) => typeof field === 'string'))) {
-    return true
-  }
-
-  return false
-}
-
-function isI18nDefinitionMap(json: I18nDefinitionMapResponse): json is I18nDefinitionMapResponse {
-  if (typeof json !== 'object' || Array.isArray(json)) {
-    return false
-  }
-  return Object.keys(json).every((key) => isi18nDefinition(json[key]))
-}
-
-function normalizeI18nDefinitionMap(response: I18nDefinitionMapResponse): I18nDefinitionMap {
-  const result = Object.fromEntries(
-    Object.entries(response).map(([key, value]) => [key, {
-      ...value,
-      translations: value.translations ?? {},
-    }]),
-  )
-
-  return result
 }
 
 export async function importTranslations(reqUrl: string | URL, base: string | URL): Promise<Translations> {
@@ -60,11 +24,10 @@ export async function importI18nJson(reqUrl: string | URL, base: string | URL): 
   const url = new URL(reqUrl, base)
   const response = await fetch(url)
   const json = await response.json()
-  if (!isI18nDefinitionMap(json)) {
-    console.error('expected json from url %o to be a map of translations, instead returned %o, returning empty translation', url.href, json)
-    return {}
-  }
-  return normalizeI18nDefinitionMap(json)
+  const normalizeResult = normalizeI18nDefinitionMap(json)
+  normalizeResult.errors.forEach((error) => console.error('Error on %o::%o, ', url, error.path, error.message))
+  normalizeResult.warnings.forEach((warning) => console.warn('Warning on %o::%o, ', url, warning.path, warning.message))
+  return normalizeI18nDefinitionMap(json).result
 }
 
 provide({
