334414c996c9841b44b815d7f6301e2880db8178
Omar Castro <omar.castro.360@gmail.com>
2023-03-31 19:35:28 +0100

    test: imrpove test readability

--- src/utils/i18n-importer/implementationn.test.ts
+++ src/utils/i18n-importer/implementationn.test.ts
@@ -1,34 +1,34 @@
 import { test } from '../../../test-utils/unit/test.ts'
 import { importI18nJson, importTranslations } from './implementation.ts'
 
-test('Given fetching a json returns a valid, normalized json, it should return the same json', async ({ expect }) => {
+test('Given `fetch` returns a valid, normalized json, `importI18nJson` should return the same json', async ({ expect }) => {
   const oldFetch = fetch
-  const expectedResult = {
+  const fetchResult = {
     'en': { 'extends': ['./translations.en.json'], translations: {} },
     'es': { 'extends': ['./translations.es.json'], translations: {} },
     'pt': { 'extends': ['./translations.pt.json'], translations: {} },
   }
   globalThis.fetch = () =>
     Promise.resolve({
-      json: () => Promise.resolve(expectedResult),
+      json: () => Promise.resolve(fetchResult),
     } as Response)
 
   const result = await importI18nJson('.', import.meta.url)
-  expect(result).toEqual(expectedResult)
+  expect(result).toEqual(fetchResult)
 
   globalThis.fetch = oldFetch
 })
 
-test('Given fetching a non normalized json returns a json without translation, it should return a normalized translations', async ({ expect }) => {
+test('Given `fetch` returns a non normalized json, `importI18nJson` should return a normalized translations', async ({ expect }) => {
   const oldFetch = fetch
-  const expectedResult = {
+  const fetchResult = {
     'en': { 'extends': './translations.en.json' },
     'es': { 'extends': './translations.es.json' },
     'pt': { 'extends': './translations.pt.json' },
   }
   globalThis.fetch = () =>
     Promise.resolve({
-      json: () => Promise.resolve(expectedResult),
+      json: () => Promise.resolve(fetchResult),
     } as Response)
 
   const result = await importI18nJson('.', import.meta.url)
