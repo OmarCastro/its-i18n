e2c7e6784bf7b47d1a7bc9dde06309b9de2494cf
Omar Castro <omar.castro.360@gmail.com>
2023-04-02 23:50:27 +0100

    test: add definition and translation test

--- src/utils/i18n-merger/implementation.test.ts
+++ src/utils/i18n-merger/implementation.test.ts
@@ -1,7 +1,7 @@
 import { test } from '../../../test-utils/unit/test.ts'
 import { builder } from './implementation.ts'
 
-test('Given a single data, merge return a normalized definition', ({ expect }) => {
+test('Given a single map, merge return a normalized definition', ({ expect }) => {
   const oldFetch = fetch
 
   const result = builder.addMap({
@@ -14,6 +14,44 @@ test('Given a single data, merge return a normalized definition', ({ expect }) =
     'en': { 'extends': ['./translations.en.json'], translations: {} },
     'es': { 'extends': ['./translations.es.json'], translations: {} },
     'pt': { 'extends': ['./translations.pt.json'], translations: {} },
+  })
+
+  globalThis.fetch = oldFetch
+})
+
+test('Given a single map and a definition in a language, merge return a normalized definition', ({ expect }) => {
+  const oldFetch = fetch
+
+  const result = builder.addMap({
+    'en': { 'extends': './translations.en.json', translations: {} },
+    'es': { 'extends': './translations.es.json', translations: {} },
+    'pt': { 'extends': './translations.pt.json', translations: {} },
+  }, 'map.all.json').addDefinitionOnLanguage('./translations.en-us.json', 'en-US', 'en-US-definition.json').build()
+
+  expect(result).toEqual({
+    'en': { 'extends': ['./translations.en.json'], translations: {} },
+    'es': { 'extends': ['./translations.es.json'], translations: {} },
+    'pt': { 'extends': ['./translations.pt.json'], translations: {} },
+    'en-US': { 'extends': ['./translations.en-us.json'], translations: {} },
+  })
+
+  globalThis.fetch = oldFetch
+})
+
+test('Given a single map and a translation in a language, merge return a normalized definition', ({ expect }) => {
+  const oldFetch = fetch
+
+  const result = builder.addMap({
+    'en': { 'extends': './translations.en.json', translations: {} },
+    'es': { 'extends': './translations.es.json', translations: {} },
+    'pt': { 'extends': './translations.pt.json', translations: {} },
+  }, 'map.all.json').addTranslations('./translations.en-us.json', 'en-US').build()
+
+  expect(result).toEqual({
+    'en': { 'extends': ['./translations.en.json'], translations: {} },
+    'es': { 'extends': ['./translations.es.json'], translations: {} },
+    'pt': { 'extends': ['./translations.pt.json'], translations: {} },
+    'en-US': { 'extends': ['./translations.en-us.json'], translations: {} },
   })
 
   globalThis.fetch = oldFetch
