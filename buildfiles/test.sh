7608cbc4e83c52615db6e79a917c39222eee7ced
Omar Castro <omar.castro.360@gmail.com>
2023-04-02 23:51:31 +0100

    test: add code coverage on tests

--- buildfiles/test.sh
+++ buildfiles/test.sh
@@ -2,5 +2,6 @@
 
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$DIR/.."
+rm -rf cov_profile
 deno test --allow-read --allow-env --allow-net --coverage=cov_profile --import-map=test-utils/unit/import_map.json src
-deno coverage --include='src/' exclude="test" cov_profile
+deno coverage --include='src/' --exclude="test" --exclude="provider.ts" --exclude="mod.ts" cov_profile
