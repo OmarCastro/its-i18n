8091cb47e89dc0b7a0161eb2ef1dce2e8508b270
Omar Castro <omar.castro.360@gmail.com>
2023-04-02 23:48:34 +0100

    src: normalize translations with i18n-normalizer

--- src/utils/i18n-importer/implementation.ts
+++ src/utils/i18n-importer/implementation.ts
@@ -1,23 +1,14 @@
 import type { I18nDefinitionMap, Translations } from './provider.ts'
-import { normalizeI18nDefinitionMap } from '../i18n-normalizer/mod.ts'
+import { normalizeI18nDefinitionMap, normalizeTranslations } from '../i18n-normalizer/mod.ts'
 import { provide } from './provider.ts'
-
-function isTranslationMap(json: Translations): json is Translations {
-  if (typeof json !== 'object' || Array.isArray(json)) {
-    return false
-  }
-  return Object.values(json).every((value) => typeof value === 'string')
-}
 
 export async function importTranslations(reqUrl: string | URL, base: string | URL): Promise<Translations> {
   const url = new URL(reqUrl, base)
   const response = await fetch(url)
   const json = await response.json()
-  if (!isTranslationMap(json)) {
-    console.error('expected json from url %o to be a map of translations, instead returned %o, returning empty translation', url.href, json)
-    return {}
-  }
-  return json
+  const normalizeResult = normalizeTranslations(json)
+  normalizeResult.errors.forEach((error) => console.error('Error on %s::%s, %s', url.href, error.path, error.message))
+  return normalizeResult.result
 }
 
 export async function importI18nJson(reqUrl: string | URL, base: string | URL): Promise<I18nDefinitionMap> {
@@ -27,7 +18,7 @@ export async function importI18nJson(reqUrl: string | URL, base: string | URL): 
   const normalizeResult = normalizeI18nDefinitionMap(json)
   normalizeResult.errors.forEach((error) => console.error('Error on %s::%s, %s', url.href, error.path, error.message))
   normalizeResult.warnings.forEach((warning) => console.warn('Warning on %s::%s, %s', url.href, warning.path, warning.message))
-  return normalizeI18nDefinitionMap(json).result
+  return normalizeResult.result
 }
 
 provide({
