1d6aa13494649289c51a430e9255b7e2bb1d1e19
Omar Castro <omar.castro.360@gmail.com>
2023-04-06 01:02:40 +0100

    test: use filesystem on html-loader.test.ts

--- src/html-loader/html-loader.test.ts
+++ src/html-loader/html-loader.test.ts
@@ -2,118 +2,75 @@ import { window } from '../../test-utils/unit/init-dom.ts'
 import { test } from '../../test-utils/unit/test.ts'
 import { loadI18n } from './html-loader.ts'
 import { provide } from '../utils/i18n-importer/provider.ts'
-import { normalizeI18nDefinitionMap } from '../utils/i18n-normalizer/mod.ts'
 
 const html = String.raw
 
-test('an HTML page with i18n-translation-map links, loadI18n should return a store', async ({ step, expect }) => {
-  const { document, location } = window
+test('an HTML page with i18n-translation-map links, loadI18n should return a store', async ({ step, expect, readFrom }) => {
+  const { document } = window
+  const location = import.meta.url
 
   document.documentElement.innerHTML = html`
     <head>
-      <link rel="i18n-locale-map" href="definition.json">
+      <link rel="i18n-locale-map" href="html-loader.test.ts--filesystem/i18n-definition-map.json">
     </head>
     <body>
       lorem ipsum
     <body>
   `
 
-  provide({
-    importI18nJson(url, base) {
-      return Promise.resolve(
-        normalizeI18nDefinitionMap({
-          'en': 'languages.en.json',
-          'es': 'languages.es.json',
-          'it': 'languages.it.json',
-          'pt': 'languages.pt.json',
-        }).result,
-      )
-    },
-    importTranslations(url, base) {
-      if (url.endsWith('languages.pt.json')) {
-        return Promise.resolve({
-          'hello world': 'olá mundo',
-          'I like red color': 'Gosto da cor vermelha',
-        })
-      }
-
-      if (url.endsWith('languages.es.json')) {
-        return Promise.resolve({
-          'hello world': 'hola mundo',
-          'I like red color': 'Me gusta la color roja',
-        })
-      }
-
-      if (url.endsWith('languages.it.json')) {
-        return Promise.resolve({
-          'hello world': 'Ciao mondo',
-          'I like red color': 'Mi piace il colore rosso',
-        })
-      }
-
-      if (url.endsWith('languages.en.json')) {
-        return Promise.resolve({
-          'hello world': 'hello world',
-          'I like red color': 'I like red color',
-        })
-      }
-
-      return Promise.resolve({})
-    },
-  })
+  provide(i18nImporterImplWith({ readFrom }))
 
   const store = await loadI18n({ document, location })
 
+  const expectedTranslationsEN = {
+    'hello world': 'hello world',
+    'I like red color': 'I like red color',
+    'I counted 0 sheeps': 'No sheeps found',
+    'I counted 1 sheeps': 'I counted 1 sheep',
+    'I counted {number} sheeps': 'I counted {0} sheeps',
+  }
+
+  const expectedTranslationsPT = {
+    'hello world': 'olá mundo',
+    'I like red color': 'Gosto da cor vermelha',
+    'I counted 0 sheeps': 'nenhuma ovelha encontrada',
+    'I counted 1 sheeps': 'contei 1 ovelha',
+    'I counted {number} sheeps': 'contei {0} ovelhas',
+  }
+
+  const expectedTranslationsES = {
+    'hello world': 'hola mundo',
+    'I like red color': 'Me gusta la color roja',
+    'I counted 0 sheeps': 'no se encontraron ovejas',
+    'I counted 1 sheeps': 'conté 1 oveja',
+    'I counted {number} sheeps': 'conté {0} ovejas',
+  }
+
   await step('where "en" locale translations are loaded correctly ', async () => {
-    expect(await store.translationsFromLanguage('en')).toEqual({
-      'hello world': 'hello world',
-      'I like red color': 'I like red color',
-    })
+    expect(await store.translationsFromLanguage('en')).toEqual(expectedTranslationsEN)
   })
 
   await step('where "en-US" locale translations, while not being loaded, still gets from "en" locale ', async () => {
-    expect(await store.translationsFromLanguage('en-US')).toEqual({
-      'hello world': 'hello world',
-      'I like red color': 'I like red color',
-    })
+    expect(await store.translationsFromLanguage('en-US')).toEqual(expectedTranslationsEN)
   })
 
   await step(
     'where "en-UK" locale translations, while not being loaded and not being valid (it is en-GB), still gets from "en" locale ',
     async () => {
-      expect(await store.translationsFromLanguage('en-UK')).toEqual({
-        'hello world': 'hello world',
-        'I like red color': 'I like red color',
-      })
+      expect(await store.translationsFromLanguage('en-UK')).toEqual(expectedTranslationsEN)
     },
   )
 
   await step('where "pt" locale translations are loaded correctly ', async () => {
-    expect(await store.translationsFromLanguage('pt')).toEqual({
-      'hello world': 'olá mundo',
-      'I like red color': 'Gosto da cor vermelha',
-    })
+    expect(await store.translationsFromLanguage('pt')).toEqual(expectedTranslationsPT)
   })
 
   await step('where "pt-BR" translations, while not being loaded, still gets from "pt" locale ', async () => {
-    expect(await store.translationsFromLanguage('pt-BR')).toEqual({
-      'hello world': 'olá mundo',
-      'I like red color': 'Gosto da cor vermelha',
-    })
+    expect(await store.translationsFromLanguage('pt-BR')).toEqual(expectedTranslationsPT)
   })
 
   await step('where "es" locale translations are loaded correctly ', async () => {
-    expect(await store.translationsFromLanguage('es')).toEqual({
-      'hello world': 'hola mundo',
-      'I like red color': 'Me gusta la color roja',
-    })
-  })
-
-  await step('where "it" locale translations are loaded correctly ', async () => {
-    expect(await store.translationsFromLanguage('it')).toEqual({
-      'hello world': 'Ciao mondo',
-      'I like red color': 'Mi piace il colore rosso',
-    })
+    expect(await store.translationsFromLanguage('es')).toEqual(expectedTranslationsES)
   })
 })
 
@@ -223,3 +180,10 @@ test('an HTML page with i18n-translation-map links, loadI18n should return a sto
     })
   })
 })
+
+function i18nImporterImplWith({ readFrom }: { readFrom: Parameters<Parameters<typeof test>[1]>[0]['readFrom'] }) {
+  return {
+    importI18nJson: async (url, base) => JSON.parse(await readFrom(new URL(url, base))),
+    importTranslations: async (url, base) => JSON.parse(await readFrom(new URL(url, base))),
+  } as Parameters<typeof provide>[0]
+}
