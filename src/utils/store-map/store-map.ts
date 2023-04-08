e8a51542e918ea58b3b12bc600bc51c17ff958e8
Omar Castro <omar.castro.360@gmail.com>
2023-04-08 11:51:17 +0100

    src: add store map

---
+++ src/utils/store-map/store-map.ts
@@ -0,0 +1,29 @@
+import { i18nTanslationStore, type TranslationStore } from '../store/translation-store.ts'
+import { traverseUpDom } from '../algorithms/traverse-up-dom.ts'
+
+const fallbackStore = i18nTanslationStore()
+const map: WeakMap<Element, TranslationStore> = new WeakMap()
+
+export const mapElementWithStore = (element: HTMLElement, store: TranslationStore) => {
+  map.set(element, store)
+}
+
+export const setStoreFromElement = (element: HTMLElement, store: TranslationStore) => {
+  map.set(element, store)
+}
+
+export const getStoresInfoFromElement = function* (target: HTMLElement) {
+  for (const element of traverseUpDom(target)) {
+    const elementStore = map.get(element)
+    if (elementStore) {
+      yield {
+        store: elementStore,
+        element,
+      }
+    }
+  }
+  yield {
+    store: fallbackStore,
+    element: null,
+  }
+}
