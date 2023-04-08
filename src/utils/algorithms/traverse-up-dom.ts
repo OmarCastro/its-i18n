14633526ebc4f38437abf3773168c0e4e8d2c802
Omar Castro <omar.castro.360@gmail.com>
2023-04-08 11:49:47 +0100

    src: create travers up DOM utility file

---
+++ src/utils/algorithms/traverse-up-dom.ts
@@ -0,0 +1,18 @@
+/**
+ *   traverse up in the document DOM closest element by passing shadow DOM
+ */
+export function* traverseUpDom(targetElement: HTMLElement = this) {
+  let el: Element | null = targetElement
+  while (el != null) {
+    yield el
+    const { parentNode } = el
+    if (parentNode instanceof ShadowRoot) {
+      el = parentNode.host
+      continue
+    }
+    if (parentNode instanceof Document) {
+      return
+    }
+    el = el.parentElement
+  }
+}
