076a84d2a00ccf8a0d2e3f64e0c1af80180a6913
Omar Castro <omar.castro.360@gmail.com>
2023-04-17 23:35:04 +0100

    add priority calculator tests

---
+++ src/key-parser/priority-calculator.test.ts
@@ -0,0 +1,23 @@
+import { test } from '../../test-utils/unit/test.ts'
+import { getAST, states } from './key-ast.util.ts'
+import { calculatePriority, getNumericValuePriority } from './priority-calculator.ts'
+
+test('Given a simple string, calculatePriority should return the max priority', ({ expect }) => {
+  const ast = getAST('hello world')
+  expect(calculatePriority(ast)).toEqual({
+    priority: [0, 0],
+    priorityAsNumber: Number.MAX_SAFE_INTEGER,
+  })
+})
+
+test('Given a simple dynamic string with "any" capture, calculatePriority should return a priority of [1, 100]', ({ expect }) => {
+  const ast = getAST('hello {}')
+  expect(calculatePriority(ast)).toEqual(withNumberiValue({
+    priority: [1, 100],
+  }))
+})
+
+const withNumberiValue = (data: { priority: readonly [number, number] }) => ({
+  ...data,
+  priorityAsNumber: getNumericValuePriority(data.priority),
+})
