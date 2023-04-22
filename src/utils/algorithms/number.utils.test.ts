1c016fa4a62482b14a00915b7895d77c394363cf
Omar Castro <omar.castro.360@gmail.com>
2023-04-22 12:55:13 +0100

    src: add number utilitities

---
+++ src/utils/algorithms/number.utils.test.ts
@@ -0,0 +1,17 @@
+import { test } from '../../../test-utils/unit/test.ts'
+import { isInteger, isNumeric } from './number.utils.ts'
+
+test('isNumeric unit tests...', ({ expect }) => {
+  expect(isNumeric({} as never)).toEqual(false)
+  expect(isNumeric('asd')).toEqual(false)
+  expect(isNumeric('12.45')).toEqual(true)
+})
+
+test('isInteger unit tests...', ({ expect }) => {
+  expect(isInteger({} as never)).toEqual(false)
+  expect(isInteger('asd')).toEqual(false)
+  expect(isInteger('12.45')).toEqual(false)
+  expect(isInteger('-1245')).toEqual(true)
+  expect(isInteger('1245')).toEqual(true)
+  expect(isInteger('0')).toEqual(true)
+})
---
+++ src/utils/algorithms/number.utils.ts
@@ -0,0 +1,7 @@
+export function isNumeric(str: string) {
+  return typeof str === 'string' && !isNaN(str as never) && !isNaN(parseFloat(str))
+}
+
+export function isInteger(str: string) {
+  return typeof str === 'string' && /^[+-]?[0-9]+$/.test(str)
+}
