93f92d919eb56483435a815c7e59d9082f2d1c05
Omar Castro <omar.castro.360@gmail.com>
2023-04-27 23:30:16 +0100

    test: add template matcher test

---
+++ src/key-parser/template-matcher.test.ts
@@ -0,0 +1,9 @@
+import { test } from '../../test-utils/unit/test.ts'
+import { getAST, states } from './key-ast.util.ts'
+import { getMatcher } from './template-matcher.ts'
+
+test('Given a simple string, getMatcher result should match the same sting only', ({ expect }) => {
+  const ast = getAST('hello world')
+  const matcher = getMatcher(ast)
+  expect(matcher('hello world')).toEqual(true)
+})
