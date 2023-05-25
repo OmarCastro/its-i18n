c87babfca0524f15c05c10f53f2316b4c545cc46
Omar Castro <omar.castro.360@gmail.com>
2023-05-25 22:21:46 +0100

    src: use breadth-first instead of querySelectorAll
    - avoids unnecessary check of already removed elements

--- src/utils/html-sanitizer/html-sanitizer.js
+++ src/utils/html-sanitizer/html-sanitizer.js
@@ -1,8 +1,9 @@
 // @ts-check
-/*
- A strictier sanitizer that allows only text related tags and attributes as to allow any sort of formatting content
-*/
-
+/**
+ *   A strictier sanitizer that allows only text related tags and attributes as to allow any sort of content formatting,
+ * makes no sense to use pictures or forms as html content in i18n, you translate mostly text, so you use data-i18n on the
+ * form elements instead of using html as translation value.
+ */
 
 /**
  * A set of allowed tags for the sanitizer decide whether to remove an element or not
@@ -100,11 +101,9 @@ export const ALLOWED_TAGS = new Set([
 ])
 
 /**
- *   A set of allowed attribute for the sanitizer decide whether to remove an
- * attribute from an element or not.
- *   Any attribute where its name is absent on the set is to be removed except
- * data-* attribute not related to i18n, and aria-* attributes
- * 
+ *   A set of allowed attribute for the sanitizer decide whether to remove an attribute from an element or not.
+ *   Any attribute where its name is absent on the set is to be removed except data-* attribute not related to i18n,
+ *  and aria-* attributes
  */
 export const ALLOWED_STANDARD_ATTRS = new Set([
   'accept',
@@ -219,15 +218,19 @@ export const ALLOWED_STANDARD_ATTRS = new Set([
 ])
 
 /**
+ * @typedef RemovedAttr
+ * @property {string}     name - removed attribte name
+ * @property {Element}    from - target Element where the attribute was removed
+ */
+
+/**
  * @typedef SanitizeHtmlResult
- * 
+ *
  * The result of `sanitizeI18nHtml`, show the sanitized html as well as the actions performed during sanitization
- * 
- * @property {string}     html                     - sanitized html result
- * @property {Element[]}  removedElements          - list of removed elements
- * @property {Object[]}   removedAttributes        - list of removed attributes
- * @property {string}     removedAttributes[].name - removed attribte
- * @property {Element}    removedAttributes[].from - target Element where the attribute was removed
+ *
+ * @property {string}        html               - sanitized html result
+ * @property {Element[]}     removedElements    - list of removed elements
+ * @property {RemovedAttr[]} removedAttributes  - list of removed attributes
  */
 
 /**
@@ -243,37 +246,48 @@ const _initDocument = (html) => {
 }
 
 /**
+ * @param {Object}        param
+ * @param {Element}       param.currentElement    - current
+ * @param {Element[]}     param.removedElements   - list of removed elements, mutable
+ * @param {RemovedAttr[]} param.removedAttributes - list of removed attributes, mutable
+ */
+const sanitizeElement = ({ currentElement, removedElements, removedAttributes }) => {
+  if (!ALLOWED_TAGS.has(currentElement.tagName.toLowerCase())) {
+    removedElements.push(currentElement)
+    currentElement.remove()
+    return
+  }
+  const { attributes } = currentElement
+  for (const attribute of Array.from(attributes)) {
+    const name = attribute.name.toLowerCase()
+    if (
+      name === 'data-i18n' ||
+      name.startsWith('data-i18n-') ||
+      (!name.startsWith('data-') && !name.startsWith('aria-') && !ALLOWED_STANDARD_ATTRS.has(name))
+    ) {
+      removedAttributes.push({ name, from: currentElement })
+      attributes.removeNamedItem(name)
+    }
+  }
+
+  for (const child of Array.from(currentElement.children)) {
+    sanitizeElement({ removedElements, removedAttributes, currentElement: child })
+  }
+}
+
+/**
  * Sanitizes The HTML input
  *
- * @param   {string}         html - HTML input string to sanitize
- * @returns {SanitizeHtmlResult}        Sanitization result
+ * @param   {string}             html - HTML input string to sanitize
+ * @returns {SanitizeHtmlResult} Sanitization result
  */
 export function sanitizeI18nHtml(html) {
-  /** @type SanitizeHtmlResult["removedElements"] */
   const removedElements = []
-  /** @type SanitizeHtmlResult["removedAttributes"] */
   const removedAttributes = []
   const doc = _initDocument(html)
-  doc.querySelectorAll('*').forEach((element) => {
-    if (!ALLOWED_TAGS.has(element.tagName.toLowerCase())) {
-      removedElements.push(element)
-      element.remove()
-      return
-    }
-    const { attributes } = element
-    for (const attribute of Array.from(attributes)) {
-      const name = attribute.name.toLowerCase()
-      if (
-        name === 'data-i18n' ||
-        name.startsWith('data-i18n-') ||
-        (!name.startsWith('data-') && !name.startsWith('aria-') && !ALLOWED_STANDARD_ATTRS.has(name))
-      ) {
-        removedAttributes.push({ name, from: element })
-        attributes.removeNamedItem(name)
-      }
-    }
-  })
-
+  for (const currentElement of Array.from(doc.children)) {
+    sanitizeElement({ removedElements, removedAttributes, currentElement })
+  }
   return {
     html: doc.innerHTML,
     removedElements,
