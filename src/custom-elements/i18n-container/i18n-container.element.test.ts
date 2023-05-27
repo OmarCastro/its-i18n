36fde80b6ed7150c78f2a3ce15387333f43edfbc
Omar Castro <omar.castro.360@gmail.com>
2023-05-27 20:49:13 +0100

    test: add conflict test

--- src/custom-elements/i18n-container/i18n-container.element.test.ts
+++ src/custom-elements/i18n-container/i18n-container.element.test.ts
@@ -26,7 +26,7 @@ function getPromiseFromEvent(item, event) {
 
 const getPromiseFrom18nApplyEvent = (item) => getPromiseFromEvent(item, 'i18n-apply')
 
-test('an HTML page with i18n-translation-map links and es lang on body, x-i18n should apply spanish i18n to its chidren correctly', async ({ step, expect, readFrom }) => {
+test('Given an HTML page with i18n-translation-map links and es lang on body, x-i18n should apply spanish i18n to its chidren correctly', async ({ step, expect, readFrom }) => {
   await defineWebComponent()
 
   const { document } = window
@@ -46,7 +46,7 @@ test('an HTML page with i18n-translation-map links and es lang on body, x-i18n s
 
   document.body.innerHTML = html`
 <${tag} class="component">
-  <span class="target-1" data-i18n--data-html="I counted 4 sheeps"></span>
+  <span class="target-1" data-i18n="I counted 4 sheeps"></span>
 </${tag}> 
 `
 
@@ -55,11 +55,11 @@ test('an HTML page with i18n-translation-map links and es lang on body, x-i18n s
 
   const target = document.querySelector('.target-1')!
 
-  await expect(target.getAttribute('data-i18n--data-html')).toEqual('I counted 4 sheeps')
-  await expect(target.getAttribute('data-html')).toEqual('conté 4 ovejas')
+  await expect(target.getAttribute('data-i18n')).toEqual('I counted 4 sheeps')
+  await expect(target.textContent).toEqual('conté 4 ovejas')
 })
 
-test('an HTML page with i18n-translation-map links and pt lang on body and en on an elment, x-i18n should apply portuguese i18n to its chidren except the one with english lang', async ({ step, expect, readFrom }) => {
+test('Given an HTML page with i18n-translation-map links and "pt" lang on body and "en" lang on an elment, x-i18n should apply portuguese i18n to its chidren except the one with english lang', async ({ step, expect, readFrom }) => {
   await defineWebComponent()
 
   const { document } = window
@@ -95,6 +95,56 @@ test('an HTML page with i18n-translation-map links and pt lang on body and en on
   await expect(targetEn.getAttribute('data-html')).toEqual('I counted 30 sheeps')
 })
 
+test('Given an element with conflicting data-i18n-* attributes, x-i18n should apply the correct attribut based on its priority', async ({ step, expect, readFrom }) => {
+  await defineWebComponent()
+
+  const { document } = window
+  const location = import.meta.url
+
+  provide(i18nImporterImplWith({ readFrom }))
+
+  document.documentElement.innerHTML = html`
+    <head>
+      <link rel="i18n-locale-map" href="i18n-container.element.test.ts--filesystem/i18n-definition-map.json">
+    </head>
+    <body lang="en"></body>
+  `
+
+  const store = await loadI18n({ document, location })
+  setStoreFromElement(document.documentElement, store)
+
+  document.body.innerHTML = html`
+<${tag} class="component">
+  <span class="target-1" data-i18n--data-attr="I counted 0 sheeps" data-i18n-attr-data-attr="I counted 4 sheeps"></span>
+  <span class="target-2" data-i18n-attr-data-attr="I counted 2 sheeps" data-i18n-attribute-data-attr="I counted 0 sheeps"></span>
+  <span class="target-3" data-i18n--data-attr="I counted 0 sheeps" data-i18n-attribute-data-attr="I counted 10 sheeps"></span>
+  <span class="target-4" data-i18n--data-attr="I counted 6 sheeps" data-i18n-attr-data-attr="I counted 4 sheeps" data-i18n-attribute-data-attr="I counted 9 sheeps"></span>
+</${tag}> 
+`
+
+  const component = document.body.querySelector('.component')!
+  await getPromiseFrom18nApplyEvent(component)
+
+  const target1 = document.querySelector('.target-1')!
+  const target2 = document.querySelector('.target-2')!
+  const target3 = document.querySelector('.target-3')!
+  const target4 = document.querySelector('.target-4')!
+
+  await step('"data-i18n-attr-*" has higher priority than "data-i18n--*"', async () => {
+    await expect(target1.getAttribute('data-attr')).toEqual('I counted 4 sheeps')
+  })
+  await step('"data-i18n-attribute-*" has higher priority than both "data-i18n-attr-*" and "data-i18n--*"', async () => {
+    await expect({
+      target2: target2.getAttribute('data-attr'),
+      target3: target3.getAttribute('data-attr'),
+      target4: target4.getAttribute('data-attr'),
+    }).toEqual({
+      target2: 'No sheeps found',
+      target3: 'I counted 10 sheeps',
+      target4: 'I counted 9 sheeps',
+    })
+  })
+})
 
 function i18nImporterImplWith({ readFrom }: { readFrom: Parameters<Parameters<typeof test>[1]>[0]['readFrom'] }) {
   return {
