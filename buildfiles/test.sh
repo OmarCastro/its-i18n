25c5ee23843a10afbf0558258c38340a3eba6343
Omar Castro <omar.castro.360@gmail.com>
2023-05-29 23:10:28 +0100

    test: run tests in parallel

--- buildfiles/test.sh
+++ buildfiles/test.sh
@@ -3,7 +3,7 @@
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$DIR/.."
 rm -rf coverage
-deno test --allow-read --allow-env --allow-net --coverage=coverage/profile --import-map=test-utils/unit/import_map.json src
+deno test --parallel --allow-read --allow-env --allow-net --coverage=coverage/profile --import-map=test-utils/unit/import_map.json src
 deno coverage --include='src/' --exclude="test" --exclude="provider.ts" --exclude="mod.ts" coverage/profile
 deno coverage --include='src/' --exclude="test" --exclude="provider.ts" --exclude="mod.ts" coverage/profile --lcov > coverage/coverage.lcov
 if type "genhtml" > /dev/null; then
