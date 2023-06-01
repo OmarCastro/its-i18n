180363afad79fde39be5012ecb2a017db4279c05
Omar Castro <omar.castro.360@gmail.com>
2023-06-01 23:52:58 +0100

    docs: document input

--- src/utils/algorithms/time.utils.ts
+++ src/utils/algorithms/time.utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Parses ISO-8601 text
+ *
+ * @param text - ISO8601 input as string
+ * @returns the representing ISO date as milliseconds from UNIX time on valid input
+ * @returns NaN on invalid input
+ */
 export function parseISO8601(text: string) {
   if (typeof text !== 'string') {
     return NaN
