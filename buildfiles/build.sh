00a6d8b6fcd301504d0b35b34dda7563103b60b0
Omar Castro <omar.castro.360@gmail.com>
2023-06-03 15:12:09 +0100

    build: build documentation on build script

--- buildfiles/build.sh
+++ buildfiles/build.sh
@@ -3,4 +3,12 @@ DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$DIR"/..
 rm -rf build
 
+
+# build dist
 buildfiles/esbuild "src/entrypoint/browser.ts" --bundle --minify --sourcemap --outfile=build/dist/i18n.element.min.js --format=esm --target=es2020 --loader:.element.html=text --loader:.element.css=text
+
+
+# build docs
+mkdir build/docs
+cp -r docs/* build/docs
+cp -r build/dist build/docs/dist
