b693d59464045e64a51e9dccc4ca787312fa070d
Omar Castro <omar.castro.360@gmail.com>
2023-06-04 23:03:09 +0100

    src: install lcov

--- buildfiles/test.sh
+++ buildfiles/test.sh
@@ -6,8 +6,6 @@ rm -rf coverage
 deno test --parallel --allow-read --allow-env --allow-net --coverage=coverage/profile --import-map=test-utils/unit/import_map.json src
 deno coverage --include='src/' --exclude="test" --exclude="provider.ts" --exclude="mod.ts" coverage/profile
 deno coverage --include='src/' --exclude="test" --exclude="provider.ts" --exclude="mod.ts" coverage/profile --lcov > coverage/coverage.lcov
-if type "genhtml" > /dev/null; then
-  genhtml -o coverage/html coverage/coverage.lcov > /dev/null
-  # build coverage badge
-  deno run --allow-read --allow-write buildfiles/build-badges.ts
-fi
+genhtml -o coverage/html coverage/coverage.lcov > /dev/null
+# build coverage badge
+deno run --allow-read --allow-write buildfiles/build-badges.ts
