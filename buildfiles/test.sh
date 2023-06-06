5c90101e245543011d2b9531808415faacf5a3a6
Omar Castro <omar.castro.360@gmail.com>
2023-06-06 23:23:02 +0100

    test: use dark theme

--- buildfiles/test.sh
+++ buildfiles/test.sh
@@ -3,9 +3,9 @@
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$DIR/.."
 rm -rf coverage
-deno test --parallel --allow-read --allow-env --allow-net --coverage=coverage/profile --import-map=test-utils/unit/import_map.json src
-deno coverage --include='src/' --exclude="test" --exclude="provider.ts" --exclude="mod.ts" coverage/profile
-deno coverage --include='src/' --exclude="test" --exclude="provider.ts" --exclude="mod.ts" coverage/profile --lcov > coverage/coverage.lcov
-genhtml -o coverage/html coverage/coverage.lcov > /dev/null
+deno test --parallel --allow-read --allow-env --allow-net --coverage=coverage/deno --import-map=test-utils/unit/import_map.json src
+deno coverage --include='src/' --exclude="test" --exclude="provider.ts" --exclude="mod.ts" coverage/deno
+deno coverage --include='src/' --exclude="test" --exclude="provider.ts" --exclude="mod.ts" coverage/deno --lcov > coverage/coverage.lcov
+genhtml -c buildfiles/gcov.css -o coverage/html coverage/coverage.lcov > /dev/null
 # build coverage badge
 deno run --allow-read --allow-write buildfiles/build-badges.ts
