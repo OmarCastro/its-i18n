f755d56432ea11214bb3011f31e132746db0c9b5
Omar Castro <omar.castro.360@gmail.com>
2023-06-12 19:48:04 +0100

    chore: lint code

--- src/utils/algorithms/regex.utils.js
+++ src/utils/algorithms/regex.utils.js
@@ -1,9 +1,9 @@
 /**
  * Escape regex pattern as to search for raw string, similar to Perl's \Q ... \E and quotemeta
- * 
- * @param {string} pattern 
+ *
+ * @param {string} pattern
  * @returns {string} escaped regex pattern
  */
-export function escape(pattern){
-    return String(pattern).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&')
+export function escape(pattern) {
+  return String(pattern).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&')
 }
