bcea249a630cb7ed3bdc7e018f33a546d2b8b0d8
Omar Castro <omar.castro.360@gmail.com>
2023-06-18 17:59:39 +0100

    build: execute build before initializing http server

--- buildfiles/dev.sh
+++ buildfiles/dev.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$DIR"/..
 
+buildfiles/build.sh
 npx http-server -b -o build/docs &
 
 while :; do 
+	inotifywait -q -r -e modify src docs
 	buildfiles/build.sh && \
 	buildfiles/format-code.sh && \
 	buildfiles/lint-code.sh
-
-	inotifywait -q -r -e modify src docs
 done
