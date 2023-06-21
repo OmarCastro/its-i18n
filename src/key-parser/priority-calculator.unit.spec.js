b043accddfa76918256e0fdd0cfee74aa476c1ae
Omar Castro <omar.castro.360@gmail.com>
2023-06-21 22:28:37 +0100

    test: refactor .test.ts to .unit.spec.js

--- src/key-parser/priority-calculator.unit.spec.js
+++ src/key-parser/priority-calculator.unit.spec.js
@@ -12,19 +12,19 @@ test('Given a simple string, calculatePriority should return the max priority', 
 
 test('Given a simple dynamic string with "any" capture, calculatePriority should return a priority of [1, 100]', ({ expect }) => {
   const ast = getAST('hello {}')
-  expect(calculatePriority(ast)).toEqual(withNumberiValue({
+  expect(calculatePriority(ast)).toEqual(withNumericValue({
     priority: [1, 100],
   }))
 })
 
 test('Given a simple dynamic string with "number" capture, calculatePriority should return a priority of [1, 100]', ({ expect }) => {
   const ast = getAST('I found { number } rocks')
-  expect(calculatePriority(ast)).toEqual(withNumberiValue({
+  expect(calculatePriority(ast)).toEqual(withNumericValue({
     priority: [1, 400],
   }))
 })
 
-const withNumberiValue = (data: { priority: readonly [number, number] }) => ({
+const withNumericValue = (data) => ({
   ...data,
   priorityAsNumber: getNumericValuePriority(data.priority),
 })
