edf646880e8ce409ffe8e2abd3c60bb7acd1603b
Omar Castro <omar.castro.360@gmail.com>
2023-06-23 00:22:52 +0100

    chore: lint code

--- src/custom-elements/i18n-container/i18n-container.element.ts
+++ src/custom-elements/i18n-container/i18n-container.element.ts
@@ -1,6 +1,6 @@
 import { eventName, observeLangFromElement } from '../../utils/algorithms/observe-lang-from-element.util.ts'
 import { getLanguageFromElement } from '../../utils/algorithms/get-lang-from-element.util.js'
-import { isElementTranslatable } from '../../utils/algorithms/get-translate-from-element.util.js' 
+import { isElementTranslatable } from '../../utils/algorithms/get-translate-from-element.util.js'
 import { getStoresInfoFromElement } from '../../utils/store-map/store-map.ts'
 import { queryFromTranslations } from '../../utils/translation-query/translation-query.util.js'
 import { sanitizeI18nHtml } from '../../utils/html-sanitizer/html-sanitizer.js'
