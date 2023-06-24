6128846ea919ea0b8484d99b38db645ae6ab6b79
Omar Castro <omar.castro.360@gmail.com>
2023-06-24 21:52:29 +0100

    chore: lint code

--- test-utils/unit/unit-test.provider.js
+++ test-utils/unit/unit-test.provider.js
@@ -1,80 +1,70 @@
 /**
  * this file adapts the test to their own environment
- * 
+ *
  * on Deno uses Deno API
  * on Node it uses playwright
  * on browser it uses a custom api
  */
 
-
-
 /** @type {(test: Readonly<Test>) => any} */
-let adapter;
+let adapter
 
-const setTestAdapter = (newAdapter) => adapter = newAdapter
+const setTestAdapter = (newAdapter) => { adapter = newAdapter }
 
 // thee 2 lines are to prevent esbuild to bundle the await imports
-const importModule = (str) => import(str) 
-let importStr;
+const importModule = (str) => import(str)
+let importStr
 const fn = async () => {
-    if(globalThis.Deno != undefined){
+  if (globalThis.Deno != undefined) {
+    // init unit tests for deno
 
-        // init unit tests for deno
-
-        importStr = 'https://deno.land/x/expect/mod.ts';
-        const { expect } = await importModule(importStr);
-        setTestAdapter(({description, test}) => {
-            globalThis.Deno.test(`${description}`, async (t) => {
-                await test({
-                    step: t.step,
-                    expect,
-                    readFrom: async (url) => await globalThis.Deno.readTextFile(url.pathname)
-                });
-            }) 
-            
+    importStr = 'https://deno.land/x/expect/mod.ts'
+    const { expect } = await importModule(importStr)
+    setTestAdapter(({ description, test }) => {
+      globalThis.Deno.test(`${description}`, async (t) => {
+        await test({
+          step: t.step,
+          expect,
+          readFrom: async (url) => await globalThis.Deno.readTextFile(url.pathname),
         })
-        return
-    }
-    if (globalThis.window == undefined) {
+      })
+    })
+    return
+  }
+  if (globalThis.window == undefined) {
+    // init unit tests for Playwright
 
-        // init unit tests for Playwright
+    importStr = '@playwright/test'
+    const { test, expect } = await importModule(importStr)
 
-
-        importStr = '@playwright/test';
-        const { test, expect } = await importModule(importStr);
-    
-        setTestAdapter(({description, test: t}) => {
-
-            test(description, async () => {
-                await t({
-                    step: test.step,
-                    expect,
-                    readFrom: async (url) => await importModule("node:fs/promises").then(({readFile}) => readFile(url.pathname))
-                });
-            }) 
+    setTestAdapter(({ description, test: t }) => {
+      test(description, async () => {
+        await t({
+          step: test.step,
+          expect,
+          readFrom: async (url) => await importModule('node:fs/promises').then(({ readFile }) => readFile(url.pathname)),
         })
-    
-    } else {
-        
-        // init unit tests to be run in browser
+      })
+    })
+  } else {
+    // init unit tests to be run in browser
 
-        const { expect } = await import('expect');
+    const { expect } = await import('expect')
 
-        
-        setTestAdapter(({description, test: t}) => {
-            console.log("-"+description)
+    setTestAdapter(({ description, test: t }) => {
+      console.log('-' + description)
 
-            return t({
+      return t({
 
-                step: async (description, test) => {
-                    console.log("--"+description)
-                    await test()
-                },
-                expect,
-                readFrom: async (url) => await fetch(url).then(req => req.text())
-            });
-        })
-    }
+        step: async (description, test) => {
+          console.log('--' + description)
+          await test()
+        },
+        expect,
+        readFrom: async (url) => await fetch(url).then(req => req.text()),
+      })
+    })
+  }
 }
 
 await fn()
@@ -83,7 +73,6 @@ await fn()
  * @param {Test} test to adapt
  */
 export const adapt = (test) => adapter(test)
-
 
 /**
  * @callback TestCall
@@ -102,6 +91,3 @@ export const adapt = (test) => adapter(test)
  * @property {string} description
  * @property {TestCall} test
  */
-
-
-
