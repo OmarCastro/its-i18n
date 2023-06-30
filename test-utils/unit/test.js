08ae5a301cbfa8a1bcc66a6b4be5290fd9711fcb
Omar Castro <omar.castro.360@gmail.com>
2023-06-30 23:26:54 +0100

    chore: lint code

--- test-utils/unit/test.js
+++ test-utils/unit/test.js
@@ -1,26 +1,23 @@
-import { adapt } from "./unit-test.provider.js";
+import { adapt } from './unit-test.provider.js'
 
 /** @type {Readonly<Test>[]} */
-const testsToExecute = [];
+const testsToExecute = []
 /** @type {*[]} */
-let adaptedtests = [];
+const adaptedtests = []
 
 /**
- * 
+ *
  * Creates a test scenario
- * 
- * @param {string} description 
- * @param {import('./unit-test.provider.js').TestCall} test 
+ *
+ * @param {string} description
+ * @param {import('./unit-test.provider.js').TestCall} test
  */
-export function test(description, test){
-  const data = Object.freeze({description, test})
+export function test (description, test) {
+  const data = Object.freeze({ description, test })
   testsToExecute.push(data)
   adaptedtests.push(adapt(data))
-
 }
 
-export function getCurrentTests(){
+export function getCurrentTests () {
   return testsToExecute.slice()
 }
-
-
