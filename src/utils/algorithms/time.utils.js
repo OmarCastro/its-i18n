a353b1eff76cd85957a92ba62e7dfef6ae4ae79b
Omar Castro <omar.castro.360@gmail.com>
2023-07-02 01:00:17 +0100

    src: add timeNowFrame

--- src/utils/algorithms/time.utils.js
+++ src/utils/algorithms/time.utils.js
@@ -12,3 +12,13 @@ export function parseISO8601 (text) {
     /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/
   return !text.match(iso8601Regex) ? NaN : Date.parse(text)
 }
+/** @type {number | null} */
+let frameTime = null
+export function timeNowFrame () {
+  if (frameTime === null) {
+    frameTime = Date.now()
+    requestAnimationFrame(() => { frameTime = null })
+  }
+
+  return frameTime
+}
