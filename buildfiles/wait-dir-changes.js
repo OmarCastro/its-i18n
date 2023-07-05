8e026d31f73587bb11ebf890252ff539428399c6
Omar Castro <omar.castro.360@gmail.com>
2023-07-06 00:08:42 +0100

    chore: lint code

--- buildfiles/wait-dir-changes.js
+++ buildfiles/wait-dir-changes.js
@@ -2,29 +2,28 @@
 import { watch } from 'node:fs'
 
 const watchers = []
-const wait = () => { if(watchers.length){ wait.timeout = setTimeout(wait, 500) } }
+const wait = () => { if (watchers.length) { wait.timeout = setTimeout(wait, 500) } }
 
 /**
  * Waits unitl any change happens in one of the defined directories
- * 
+ *
  * @param {string} dirs - directories to watch for changes
- * @returns 
+ * @returns
  */
-function waitOneOfDirChanges(...dirs){
-	if(!watchers.length){
-		setImmediate(wait)
-	}
-	for(const dir of dirs){
-		watchers.push(watch(dir, { recursive: true }, (eventType, filename) => {
-			console.log(`${eventType} ${filename}`)
-			watchers.forEach(watcher => watcher.close())
-			clearTimeout(wait.timeout)
-		}));
-	}
+function waitOneOfDirChanges (...dirs) {
+  if (!watchers.length) {
+    setImmediate(wait)
+  }
+  for (const dir of dirs) {
+    watchers.push(watch(dir, { recursive: true }, (eventType, filename) => {
+      console.log(`${eventType} ${filename}`)
+      watchers.forEach(watcher => watcher.close())
+      clearTimeout(wait.timeout)
+    }))
+  }
 }
 
 waitOneOfDirChanges(
-	new URL('../src',import.meta.url), 
-	new URL('../docs',import.meta.url)
+  new URL('../src', import.meta.url),
+  new URL('../docs', import.meta.url),
 )
-
