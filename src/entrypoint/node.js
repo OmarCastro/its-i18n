eb9386bd69b9aefb6127d9a4037ecb1eee8bf415
Omar Castro <omar.castro.360@gmail.com>
2023-07-15 23:19:05 +0100

    add nodejs entrypoint

---
+++ src/entrypoint/node.js
@@ -0,0 +1,11 @@
+import I18nElement from '../custom-elements/i18n-container/i18n-container.element.js'
+
+const url = new URL(import.meta.url)
+const elementName = url.searchParams.get('named')
+if (elementName) {
+  if (customElements.get(elementName) != null) {
+    console.error(`A custom element with name "${elementName}" already exists`)
+  } else {
+    customElements.define(elementName, I18nElement)
+  }
+}
