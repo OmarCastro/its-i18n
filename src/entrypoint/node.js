2cfd281bd143c31ee68d85e4a3246178885b91bf
Omar Castro <omar.castro.360@gmail.com>
2023-07-16 18:21:45 +0100

    fix: fix nodejs entrypoint not working correctly

--- src/entrypoint/node.js
+++ src/entrypoint/node.js
@@ -1,12 +1,4 @@
 import '../utils/i18n-importer/implementation.js'
-import I18nElement from '../custom-elements/i18n-container/i18n-container.element.js'
-
-const url = new URL(import.meta.url)
-const elementName = url.searchParams.get('named')
-if (elementName) {
-  if (customElements.get(elementName) != null) {
-    console.error(`A custom element with name "${elementName}" already exists`)
-  } else {
-    customElements.define(elementName, I18nElement)
-  }
-}
+import { I18nContainerElement as element } from '../custom-elements/i18n-container/i18n-container.element.js'
+export const I18nContainerElement = element
+export default I18nContainerElement
