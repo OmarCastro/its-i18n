b9a982ab7d618fbf3009a89625a49852abae956f
Omar Castro <omar.castro.360@gmail.com>
2023-07-22 17:13:50 +0100

    test: enable parallel tests

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ const config = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'never', outputFolder: 'reports/playwright-report'  }],
