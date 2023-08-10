044fcd98710c4ab931f97577d5f4e79fa441d2ad
Omar Castro <omar.castro.360@gmail.com>
2023-08-11 00:12:01 +0100

    src: update imports

--- src/entrypoint/browser.js
+++ src/entrypoint/browser.js
@@ -1,5 +1,7 @@
 import '../utils/i18n-importer/implementation.js'
-import I18nElement from '../custom-elements/i18n-container/i18n-container.element.js'
+import { I18nContainerElement as Element } from '../custom-elements/i18n-container/i18n-container.element.js'
+export { i18n } from '../js-api/api.js'
+export { I18nContainerElement } from '../custom-elements/i18n-container/i18n-container.element.js'
 
 const url = new URL(import.meta.url)
 const elementName = url.searchParams.get('named')
@@ -7,8 +9,6 @@ if (elementName) {
   if (customElements.get(elementName) != null) {
     console.error(`A custom element with name "${elementName}" already exists`)
   } else {
-    customElements.define(elementName, I18nElement)
+    customElements.define(elementName, Element)
   }
 }
-
-export default I18nElement
