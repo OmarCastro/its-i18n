fc71043918096ed91e3d6356fedd255bbe0a2bff
OmarCastro <omar.castro.360@gmail.com>
2026-07-09 23:58:26 +0100

    refactor: improve tick-time code

--- src/utils/tick-time/tick-time.js
+++ src/utils/tick-time/tick-time.js
@@ -11,7 +11,7 @@ const MARGIN_MILLIS = 100
 
 let timeTickInstances
 /**
- * @param {TimeTickInstance} newInstance - target time tick instance
+ * @param {TimeTickInstance["data"]} newInstance - target time tick instance
  */
 export function checkTick (newInstance) {
   const { callbacks, tickingElements, timeoutNumber } = newInstance
@@ -30,70 +30,65 @@ export function checkTick (newInstance) {
   }
 }
 
-/**
- * @this {TimeTickInstance}
- * @param {Element} element - target element
- */
-export function tickElement (element) {
-  this.tickingElements.add(element)
-  checkTick(this)
-}
 
 /**
- * @this {TimeTickInstance}
- * @param {Element} element - target element
- */
-export function untickElement (element) {
-  this.tickingElements.add(element)
-}
-
-/**
- * @this {TimeTickInstance}
- * @param {TimeTickCallback} callback - callback to trigger each time tick
- */
-export function addCallback (callback) {
-  this.callbacks.add(callback)
-  checkTick(this)
-}
-
-/**
- * @this {TimeTickInstance}
- * @param {TimeTickCallback} callback - callback added on {@link addCallback}
- */
-export function removeCallback (callback) {
-  this.callbacks.delete(callback)
-}
-
-/**
- * @class
  * Builds a time tick instance
  */
-function TimeTickInstance () {
-  /** @type {IterableWeakSet<Element>} */
-  this.tickingElements = new IterableWeakSet()
-  /** @type {unknown} */
-  this.timeoutNumber = undefined
-  /** @type {Set<TimeTickCallback>} */
-  this.callbacks = new Set()
-}
+function buildTimeTick () {
+  const data = {
+    /** @type {IterableWeakSet<Element>} */
+    tickingElements: new IterableWeakSet(),
+    /** @type {unknown} */
+    timeoutNumber: undefined,
+    /** @type {Set<TimeTickCallback>} */
+    callbacks: new Set(),
+  }
 
-TimeTickInstance.prototype.tickElement = tickElement
-TimeTickInstance.prototype.untickElement = untickElement
-TimeTickInstance.prototype.addCallback = addCallback
-TimeTickInstance.prototype.removeCallback = removeCallback
+  const api = {
+
+    /** @param {Element} element - target element */
+    tickElement: element => {
+      data.tickingElements.add(element)
+      checkTick(data)
+    },
+
+    /** @param {Element} element - target element */
+    untickElement: element => {
+      data.tickingElements.delete(element)
+    },
+
+    /** @param {TimeTickCallback} callback - callback to trigger each time tick */
+    addCallback: callback => {
+      data.callbacks.add(callback)
+      checkTick(data)
+    },
+
+     /** @param {TimeTickCallback} callback - callback to trigger each time tick */
+    removeCallback: callback => {
+      data.callbacks.delete(callback)
+    }
+  }
+
+  return { api, data }
+}
+  
 
 /**
- * @returns {TimeTickInstance} singleton time tick instance
+ * @returns {TimeTickInstance["api"]} singleton time tick instance
  */
 export function timeTick () {
-  timeTickInstances ??= new TimeTickInstance()
-  return timeTickInstances
+  timeTickInstances ??= buildTimeTick()
+  return timeTickInstances.api
 }
 
 /**
  * @typedef {object} TimeTickCallbackParams
  * @property {Element[]} targets - target ticking elements
  * @property {(el: Element) => void} untick - stop ticking an element, does nothing if already not ticking
+ */
+
+/**
+ * @typedef {ReturnType<typeof buildTimeTick>} TimeTickInstance
  */
 
 /**
